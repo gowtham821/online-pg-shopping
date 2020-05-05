d2aa12e7b05dfc85fe4192c888f8d44b91985b53
pgowtham <gowtham821@gmail.com>
2020-05-05 17:05:55 +0530

    success

---
+++ shopping/src/main/java/pg/shopping/controller/PageController.java
@@ -0,0 +1,19 @@
+package pg.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/","home","index"})
+	public ModelAndView index() {
+		ModelAndView mv= new ModelAndView("page");
+			mv.addObject("Greeting","Welcome To Spring WebMVC");
+	return mv;			
+		
+	}
+
+	
+}
